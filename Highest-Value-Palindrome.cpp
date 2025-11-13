87ff0f12fd041e4ce367fb7eaacf93c9b5aa1ecb
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-13 09:45:00 +0900

    Remove unused string trimming functions
    Removed unused ltrim and rtrim functions to clean up code.

--- Highest-Value-Palindrome.cpp
+++ Highest-Value-Palindrome.cpp
@@ -51,39 +51,6 @@ string ltrim(const string &str)
     return s;
 }
 
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-
-string rtrim(const string &str)
-{
-    string s(str);
-    s.erase(
-        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
-    );
-    return s;
-}
-
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
 vector<string> split(const string &str) {
     vector<string> tokens;
 

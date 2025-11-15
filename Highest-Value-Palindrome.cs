ec4f3fc26ceb9f437fbb0029de36bf2b4d82f0ab
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-15 11:09:46 +0900

    Update Highest-Value-Palindrome.cs

--- Highest-Value-Palindrome.cs
+++ Highest-Value-Palindrome.cs
@@ -11,3 +11,23 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'highestValuePalindrome' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER n
+     *  3. INTEGER k
+     */
+
+    public static string highestValuePalindrome(string s, int n, int k)
+    {
+
+    }
+
+}

968a1df2c25a477f5f40c9b03f44c691dd9df48c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-18 10:22:50 +0900

    Update Highest-Value-Palindrome.java

--- Highest-Value-Palindrome.java
+++ Highest-Value-Palindrome.java
@@ -9,3 +9,23 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+
+class Result {
+
+    /*
+     * Complete the 'highestValuePalindrome' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER n
+     *  3. INTEGER k
+     */
+
+    public static String highestValuePalindrome(String s, int n, int k) {
+    // Write your code here
+
+    }
+
+}

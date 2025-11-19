284df530616399cb7af753d507afe21192b03c2f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-19 12:09:00 +0900

    Update Highest-Value-Palindrome.java

--- Highest-Value-Palindrome.java
+++ Highest-Value-Palindrome.java
@@ -29,3 +29,28 @@ class Result {
     }
 
 }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.highestValuePalindrome(s, n, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

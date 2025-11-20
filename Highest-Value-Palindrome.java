b47da89fa7924ae09549f8b9a24a2639669a3c4d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-20 12:45:36 +0900

    Update Highest-Value-Palindrome.java

--- Highest-Value-Palindrome.java
+++ Highest-Value-Palindrome.java
@@ -10,7 +10,6 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
 class Result {
 
     /*
@@ -24,13 +23,56 @@ class Result {
      */
 
     public static String highestValuePalindrome(String s, int n, int k) {
-    // Write your code here
+        char[] arr = s.toCharArray();
+        boolean[] changed = new boolean[n];
+        int left = 0, right = n - 1;
+        int used = 0;
 
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                if (arr[left] > arr[right])
+                    arr[right] = arr[left];
+                else
+                    arr[left] = arr[right];
+                changed[left] = changed[right] = true;
+                used++;
+            }
+            left++;
+            right--;
+        }
+
+        if (used > k) return "-1";
+
+        int remain = k - used;
+        left = 0;
+        right = n - 1;
+
+        while (left <= right) {
+            if (left == right) {
+                if (remain > 0 && arr[left] != '9') arr[left] = '9';
+            } else {
+                if (arr[left] != '9') {
+                    if (changed[left] || changed[right]) {
+                        if (remain >= 1) {
+                            arr[left] = arr[right] = '9';
+                            remain -= 1;
+                        }
+                    } else {
+                        if (remain >= 2) {
+                            arr[left] = arr[right] = '9';
+                            remain -= 2;
+                        }
+                    }
+                }
+            }
+            left++;
+            right--;
+        }
+
+        return new String(arr);
     }
 
 }
-
-
 
 public class Solution {
     public static void main(String[] args) throws IOException {
